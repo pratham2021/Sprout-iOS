93a89dcc0eefc5be4aec81bb2206a828c78ce8ff
Pratham Hebbar <prathamhebbar2021@gmail.com>
2025-08-05 16:08:43 -0500

    App Icon and Basic Tab View

---
+++ Sprout/SavedView.swift
@@ -0,0 +1,18 @@
+//
+//  SavedView.swift
+//  Sprout
+//
+//  Created by Pratham  Hebbar on 8/5/25.
+//
+
+import SwiftUI
+
+struct SavedView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SavedView()
+}
