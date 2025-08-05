526637b51382ed395b8143df3ffcd1dfe15adf02
Pratham Hebbar <prathamhebbar2021@gmail.com>
2025-08-05 16:00:15 -0500

    Initial Commit

---
+++ Sprout/SproutApp.swift
@@ -0,0 +1,17 @@
+//
+//  SproutApp.swift
+//  Sprout
+//
+//  Created by Pratham  Hebbar on 8/5/25.
+//
+
+import SwiftUI
+
+@main
+struct SproutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
