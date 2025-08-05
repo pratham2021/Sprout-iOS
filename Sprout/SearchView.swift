d9ab9f6be07b6d8ad3bf363ba70ccfc89e92d92e
Pratham Hebbar <prathamhebbar2021@gmail.com>
2025-08-05 18:16:10 -0500

    trivial changes

--- Sprout/SearchView.swift
+++ Sprout/SearchView.swift
@@ -1,9 +1,6 @@
-//
 //  SearchView.swift
 //  Sprout
-//
 //  Created by Pratham  Hebbar on 8/5/25.
-//
 
 import SwiftUI
 
@@ -21,9 +18,6 @@ struct SearchView: View {
                 .multilineTextAlignment(.center)
         }
         .background(colorScheme == .light ? Color(red: 250/255, green: 187/255, blue: 139/255).ignoresSafeArea() : Color(red: 244/255, green: 218/255, blue: 198/255).ignoresSafeArea())
-        
-        
-
     }
 }
 
