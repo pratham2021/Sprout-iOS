08b8dadce713cc29b8b8e45f59ded5567101e60a
Pratham Hebbar <prathamhebbar2021@gmail.com>
2025-08-09 16:56:41 -0500

    push

--- Sprout/Data Model/Utilities.swift
+++ Sprout/Data Model/Utilities.swift
@@ -7,8 +7,4 @@
 
 import Foundation
 
-//var PERENUAL_API_KEY = "sk-0fqM689753b9c590a11727"
-
 var RAPID_API_KEY = "f45df8004cmshcd8669a8fb60f20p1b1e92jsn367ae8790e60"
-
-//let urlString = "https://perenual.com/api/v2/species/details/20?key=\(PERENUAL_API_KEY)"
