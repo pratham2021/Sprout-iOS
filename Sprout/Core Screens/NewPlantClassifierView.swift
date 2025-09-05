0af18a1fccf327a9e84d3ddefa4a93112c856983
Pratham Hebbar <prathamhebbar2021@gmail.com>
2025-09-05 14:13:01 -0500

    work in progress

--- Sprout/Core Screens/NewPlantClassifierView.swift
+++ Sprout/Core Screens/NewPlantClassifierView.swift
@@ -2,9 +2,7 @@
 //  NewPlantClassifierView.swift
 //  Sprout
 //
-//  Created by Pratham  Hebbar on 9/5/25.
-
-
+//  Created by Pratham  Hebbar on 9/5/2025.
 //
 
 import SwiftUI
@@ -14,84 +12,82 @@ import PhotosUI
 struct NewPlantClassifierView: View {
     
     @Environment(\.colorScheme) var colorScheme
-//    Query(sort: \LocalPlantPrediction.name) var samples: [LocalPlantPrediction]
-//    @Environment(\.modelContext) private var modelContext
-    @State var samples: [LocalPlantPrediction] = []
+//    Query(sort: \LocalPlantPrediction.name) var localPlantPredictions: [LocalPlantPrediction]
+    @Environment(\.modelContext) private var modelContext
+    @State var localPlantPredictions: [LocalPlantPrediction] = []
     @State var showLibrary: Bool = false
-    @State private var selectedItem: PhotosPickerItem? // holds the selected photo item
+    @State private var photosPickerItem: PhotosPickerItem? // holds the selected photo item
     @State private var selectedImage: UIImage?
     @State private var showingCamera: Bool = false
-    @State private var showingPhotoLibrary: Bool = false
 
     var body: some View {
         ZStack {
             backgroundColor.ignoresSafeArea()
-
-            VStack {
+            
+            NavigationStack {
                 
-                HStack {
+                VStack {
+                    List {
+                        
+                    }
+                    .scrollContentBackground(.hidden)
+                    .background(Color.clear)
+                    .listStyle(.plain)
+                    .environment(\.editMode, .constant(.inactive))
+                    .overlay {
+                        if localPlantPredictions.isEmpty {
+                            emptyStateView
+                        }
+                    }
+                }
+                .padding()
+                .navigationTitle(
                     Text("Plant Predictions")
                         .foregroundColor(textColor)
                         .font(.title)
                         .fontWeight(.bold)
                         .textCase(nil)
-                    
-                    Spacer()
-                    
-                    HStack(spacing: 20) {
-                        
-                        
-                        Button {
+                )
+                .toolbar {
+                    ToolbarItem(placement: .topBarTrailing) {
+                        Button(action: {
                             showingCamera = true
-                        } label: {
+                        }, label: {
                             Image(systemName: "camera")
                                 .symbolRenderingMode(.monochrome)
                                 .foregroundStyle(textColor)
-                        }
+                        })
                         .buttonStyle(PlainButtonStyle())
-                        .fullScreenCover(isPresented: $showingCamera) {
-                            CameraView(image: $selectedImage)
-                        }
-                        
-                        Button {
-                            
-                        } label: {
+                    }
+                    
+                    ToolbarItem(placement: .topBarTrailing) {
+                        PhotosPicker(selection: $photosPickerItem, matching: .images) {
                             Image(systemName: "photo")
                                 .symbolRenderingMode(.monochrome)
                                 .foregroundStyle(textColor)
                         }
                         .buttonStyle(PlainButtonStyle())
-                        .fullScreenCover(isPresented: $showingPhotoLibrary) {
-                           // Image picker
-                        }
-                        
-                    }
-                    
-                }
-                // .padding()
-                
-                
-                List {
-                    
-                }
-                .scrollContentBackground(.hidden)
-                .background(Color.clear)
-                .listStyle(.plain)
-                .environment(\.editMode, .constant(.inactive))
-                .overlay {
-                    if samples.isEmpty {
-                        emptyStateView
                     }
                 }
             }
-            .padding()
-        }
-        .onChange(of: selectedImage) { newImage in
-            if newImage != nil {
-                print("Selected Image")
-                print(newImage!)
+            .fullScreenCover(isPresented: $showingCamera) {
+                CameraView(image: $selectedImage)
             }
         }
+        .background(Color.clear)
+        .onChange(of: photosPickerItem) { _, _ in
+            Task {
+                if let photosPickerItem, let data = try? await photosPickerItem.loadTransferable(type: Data.self) {
+                    if let image = UIImage(data: data) {
+                        selectedImage = image
+                    }
+                }
+                
+                photosPickerItem = nil
+            }
+        }
+        
+
         
     }
     
